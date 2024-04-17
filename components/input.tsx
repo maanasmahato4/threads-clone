eb71442d24ce3bc390cb0c76063a8fefb32727f5
maanasmahato4 <maanasmahato456@gmail.com>
2024-04-18 03:23:24 +0545

    [update]: signup form schema validation

--- components/input.tsx
+++ components/input.tsx
@@ -6,6 +6,7 @@ interface InputProps {
 	type?: string;
 	value: string;
 	label: string;
+	errorMessage?: string;
 }
 
 export const Input: React.FC<InputProps> = ({
@@ -14,17 +15,21 @@ export const Input: React.FC<InputProps> = ({
 	type = 'text',
 	value,
 	label,
+	errorMessage,
 }): React.ReactElement => {
 	return (
 		<div className='relative'>
 			<input
-				className='peer block w-full appearance-none rounded-md bg-neutral-700 pb-1 ps-6 pt-6 invalid:border-b-2 focus:outline-none focus:ring-0'
+				className='peer block w-full appearance-none rounded-md border-gray-500 border-2 pb-1 ps-6 pt-6 invalid:border-b-2 focus:outline-none focus:ring-0'
 				onChange={onChange}
 				type={type}
 				value={value}
 				id={id}
 				placeholder=' '
 			/>
+			{errorMessage && errorMessage != '' ? (
+				<span className='text-red-600'>{errorMessage}</span>
+			) : null}
 			<label
 				htmlFor={id}
 				className='absolute left-6 top-4 z-10 origin-[0] -translate-y-3 scale-75 transform text-zinc-400 duration-150 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-3  peer-focus:scale-75'
--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -1,8 +1,39 @@
 'use client';
 
+import { z } from 'zod';
+
 import { Input } from './input';
 import { Button } from './ui/button';
 import { Checkbox } from './ui/checkbox';
+
+const FormSchema = z
+	.object({
+		username: z
+			.string()
+			.min(6, 'username must be atleast 6 characters')
+			.max(40, 'username must at max 40 characters')
+			.regex(new RegExp('^[a-zA-Z]+$'), 'no special characters allowed'),
+		email: z.string().email('please enter a valid email'),
+		password: z
+			.string()
+			.min(6, 'Password must be at least 6 characters ')
+			.max(50, 'Password must be less than 50 characters'),
+		confirmPassword: z
+			.string()
+			.min(6, 'Password must be at least 6 characters ')
+			.max(50, 'Password must be less than 50 characters'),
+		accepted: z.literal(true, {
+			errorMap: () => ({
+				message: 'please accept all terms',
+			}),
+		}),
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		message: 'password and confirm password does not match',
+		path: ['confirmPassword'],
+	});
+
+type InputType = z.infer<typeof FormSchema>;
 
 export default function SignUpForm() {
 	const handleFormSubmit = async () => {};
