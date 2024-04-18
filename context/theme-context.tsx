645375062cf26dd86bc2581915f3e7622f152f00
maanasmahato4 <maanasmahato456@gmail.com>
2024-04-19 03:49:53 +0545

    [new]: theme context

---
+++ context/theme-context.tsx
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from 'react';
+
+interface ThemeContextProps {
+	theme: string;
+	setTheme: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const ThemeContext = createContext<ThemeContextProps>({
+	theme: '',
+	setTheme: () => {},
+});
+
+export const ThemeContextProvider = ({
+	children,
+}: {
+	children: React.ReactNode;
+}) => {
+	const [theme, setTheme] = useState<string>('');
+	return (
+		<ThemeContext.Provider value={{ theme, setTheme }}>
+			{children}
+		</ThemeContext.Provider>
+	);
+};
