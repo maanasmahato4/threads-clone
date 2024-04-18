81dd84318894385ddb64de62c57f456e7c82c9bd
maanasmahato4 <maanasmahato456@gmail.com>
2024-04-18 18:53:34 +0545

    [update]: auth done

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,3 @@
-import React from 'react';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '../api/auth/[...nextauth]/route';
-
 async function Home() {
 	return <div>Home</div>;
 }
