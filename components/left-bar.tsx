ef218374a6f7a3c2e55a7a788de1a591316e3f46
maanasmahato4 <maanasmahato456@gmail.com>
2024-04-21 00:37:30 +0545

    [update]: left bar links and icons

--- components/left-bar.tsx
+++ components/left-bar.tsx
@@ -2,6 +2,9 @@
 
 import { ThemeContext } from '@/context/theme-context';
 import { useContext } from 'react';
+import { leftBarLinks } from '@/lib/contants';
+import Link from 'next/link';
+import Image from 'next/image';
 
 interface LeftBarProps {
 	className?: string;
@@ -13,7 +16,18 @@ const LeftBar: React.FC<LeftBarProps> = ({ className }) => {
 		<div
 			className={`${className} ${theme === 'dark' ? 'bg-zinc-900' : 'bg-white'}`}
 		>
-			left bar
+			{leftBarLinks.map((link, idx) => {
+				return (
+					<Link
+						key={idx}
+						href={link.route}
+						className='flex gap-x-2 py-4 flex-row pl-4 m-2 items-center hover:bg-purple-600 rounded-md active:bg-purple-600 focus:bg-purple-600'
+					>
+						<Image height={24} width={24} src={link.imgURL} alt='' />{' '}
+						{link.label}
+					</Link>
+				);
+			})}
 		</div>
 	);
 };
